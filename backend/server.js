7f3bb487b4290581e2391f300c7e2270d815bc13
Dan Rufener <daniel.rufener@gmail.com>
2024-06-15 13:29:55 -0700

    date files

--- backend/server.js
+++ backend/server.js
@@ -1,45 +1,40 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const cors = require('cors')
+const moment = require('moment-timezone');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-
-app.use(cors())
+app.use(cors());
 
 app.get('/punches', (req, res) => {
+    const filePath = getFilePath();
 
     fs.readFile(filePath, 'utf8', (err, data) => {
         if (err) {
             if (err.code === 'ENOENT') {
-                var punches = [];
+                return res.send([]);
             } else {
                 return res.status(500).send('Error reading file');
             }
-        } else {
-            try {
-                punches = JSON.parse(data);
-            } catch (parseErr) {
-                return res.status(500).send('Error parsing JSON data');
-            }
         }
-    })
-    res.send(punches);
-})
+        try {
+            const punches = JSON.parse(data);
+            res.send(punches);
+        } catch (parseErr) {
+            return res.status(500).send('Error parsing JSON data');
+        }
+    });
+});
 
 app.post('/punch', (req, res) => {
     const punch = req.body;
-    const filePath = path.join(__dirname, 'timesheet.json');
+    const filePath = getFilePath();
 
     fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) {
-            if (err.code === 'ENOENT') {
-                var punches = [];
-            } else {
-                return res.status(500).send('Error reading file');
-            }
-        } else {
+        let punches = [];
+        if (!err) {
             try {
                 punches = JSON.parse(data);
             } catch (parseErr) {
@@ -47,18 +42,22 @@ app.post('/punch', (req, res) => {
             }
         }
 
-        // Append the new punch
         punches.push(punch);
 
-        // Write the updated timesheet back to the file
         fs.writeFile(filePath, JSON.stringify(punches, null, 2), (err) => {
             if (err) {
-                return res.status(500).send('Error writing file');
+                return res.status(500).send(`Error writing file ${err}`);
             }
             res.send(punches);
         });
     });
 });
+
+function getFilePath() {
+    const currentDate = moment().tz('America/Los_Angeles');
+    const filename = currentDate.format('YYYYMMDD') + '_timesheet.json';
+    return path.join(__dirname, "data", filename);
+}
 
 app.listen(5001, () => {
     console.log('Server is running on port 5001');
